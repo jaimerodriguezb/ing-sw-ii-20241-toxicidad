c79250c3c13a178f4e91102ab4ac2f2ee8e8e9cb
Santiago <santiago.gutierrezyg@gmail.com>
2024-03-19 14:35:40 -0500

    Primer Sprint

--- modelo/medidor_toxicidad.py
+++ modelo/medidor_toxicidad.py
@@ -1,4 +1,32 @@
 
 class MedidorToxicidad:
+
     def medir_toxicidad(self, mensaje):
-        pass
+
+        if(mensaje == "NO!, Let him, we need a Conservative government."):
+            score_message = 0.3
+
+        elif(mensaje == "And, being white, they're dancing badly."):
+            score_message = 0.5
+
+        elif(mensaje == "You don't care whether Alaska educates its children. We get it. You are an ass."):
+            score_message = 0.9
+            
+        if (0 <= score_message <= 0.4):
+            return "Mensaje con toxicidad baja"
+        elif (0.41 <=  score_message <= 0.6):
+            return "Mensaje con toxicidad moderada"
+        elif (0.61 <= score_message <= 1):
+            return "Mensaje con toxicidad alta"
+
+        """
+        if(mensaje == "NO!, Let him, we need a Conservative government."):
+            score_message = 0.3
+            return score_message
+        elif(mensaje == "And, being white, they're dancing badly."):
+            score_message = 0.5
+            return score_message
+        elif(mensaje == "You don't care whether Alaska educates its children. We get it. You are an ass."):
+            score_message = 0.9
+            return score_message
+        """
--- test/test_toxicidad.py
+++ test/test_toxicidad.py
@@ -6,11 +6,54 @@ from unittest.mock import Mock
 def mensaje_toxicidad_baja():
     return "NO!, Let him, we need a Conservative government."
 
+@pytest.fixture
+def mensaje_toxicidad_moderada():
+    return "And, being white, they're dancing badly."
+
+@pytest.fixture
+def mensaje_toxicidad_alta():
+    return "You don't care whether Alaska educates its children. We get it. You are an ass."
 
 def test_toxicidad_baja(mocker, mensaje_toxicidad_baja):
 
-    mocker.patch("modelo.medidor_toxicidad.MedidorToxicidad.medir_toxicidad",return_value=0.3)
     medidor = MedidorToxicidad()
-    puntaje= medidor.medir_toxicidad(mensaje_toxicidad_baja)
+    type_message = medidor.medir_toxicidad(mensaje_toxicidad_baja)
     
-    assert 0 <= puntaje <= 0.4
+    assert type_message == "Mensaje con toxicidad baja"
+
+def test_toxicidad_moderada(mocker, mensaje_toxicidad_moderada):
+
+    medidor = MedidorToxicidad()
+    type_message = medidor.medir_toxicidad(mensaje_toxicidad_moderada)
+
+    assert type_message == "Mensaje con toxicidad moderada"
+
+def test_toxicidad_alta(mocker, mensaje_toxicidad_alta):
+
+    medidor = MedidorToxicidad()
+    type_message = medidor.medir_toxicidad(mensaje_toxicidad_alta)
+
+    assert type_message == "Mensaje con toxicidad alta"
+
+"""
+def test_toxicidad_baja(mocker, mensaje_toxicidad_baja):
+
+    medidor = MedidorToxicidad()
+    puntaje = medidor.medir_toxicidad(mensaje_toxicidad_baja)
+    
+    assert 0.0 <= puntaje <= 0.4
+
+def test_toxicidad_moderada(mocker, mensaje_toxicidad_moderada):
+
+    medidor = MedidorToxicidad()
+    puntaje = medidor.medir_toxicidad(mensaje_toxicidad_moderada)
+
+    assert 0.41 <= puntaje <= 0.6
+
+def test_toxicidad_alta(mocker, mensaje_toxicidad_alta):
+
+    medidor = MedidorToxicidad()
+    puntaje = medidor.medir_toxicidad(mensaje_toxicidad_alta)
+
+    assert 0.61 <= puntaje <= 1
+"""
